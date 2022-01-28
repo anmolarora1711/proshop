076807bc727e9191c4b254dc99c3b14cc07afc45
Anmol Arora <anmol1610140@gmail.com>
2022-01-28 15:53:24 +0530

    Adding Colors to the Console

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,9 +7,9 @@ const connectDB = async () => {
       useNewUrlParser: true
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 }
