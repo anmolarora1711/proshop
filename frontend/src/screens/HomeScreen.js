fe544739c12f07b2eb164fdecff74eba57168b61
Anmol Arora <anmol1610140@gmail.com>
2022-01-28 11:32:42 +0530

    Rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,13 +4,12 @@ import Product from '../components/Product';
 import products from '../products';
 
 const HomeScreen = () => {
-  console.log(products);
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
